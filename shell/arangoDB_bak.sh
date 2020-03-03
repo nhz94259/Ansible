df4c1fa03a5fcbc3b3f4a91c4f49963dd4c1aa80
nhz94259 <nhz94259@163.com>
2020-03-03 19:13:55 +0800

    本脚本用于docker 下的arango数据库导出导入备份，在tar包中提供了一个协助导入的脚本
    本脚本用于docker 下的arango数据库导出导入备份，在tar包中提供了一个协助导入的脚本

--- shell/arangoDB_bak.sh
+++ shell/arangoDB_bak.sh
@@ -11,11 +11,12 @@ containerIdorName=arangodb_app_1
 arango_volunm_dir=/opt/cloudnet/arangodb/data/ #docker arango volumn dir
 cur_shell_dir=`pwd`
 work_dir=$cur_shell_dir'/cmdb_data_backup/'
-echo $work_dir
+filename=$database_name'-dump-'$date
+
 if [ ! -d $work_dir  ];then
     mkdir $work_dir
 fi
-filename='dump-'$date
+
 docker exec  $containerIdorName  sh -c 'arangodump --server.endpoint tcp://localhost:8529 --server.username '$data_user' --server.password '$data_password' --server.database '$database_name' --output-directory /var/lib/arangodb3/'$filename'  --overwrite true'
 if [ $? -eq 0 ]; then
      echo -e "\033[32m"backup succeed "\033[0m"
@@ -29,16 +30,7 @@ EOF
      echo
      echo -e "\033[32m"The latest backup file is : $work_dir$filename.tar"\033[0m"
      echo -e "\033[32m"You can use follow command to check it: tar -tf $work_dir$filename.tar"\033[0m"
-     echo -e "\033[32m"We provide scripts to support arangodb data import in $filename.tar , aarangorestore.sh"\033[0m"
-    
+     echo -e "\033[32m"We provide scripts to support arangodb data import in $filename.tar , aarangorestore.sh"\033[0m" 
 else
-     echo -e "\033[1;5;31mbackup faild\033[0m"
-     
+     echo -e "\033[1;5;31mbackup faild\033[0m"  
 fi
-
-
-
-
-
-
-
