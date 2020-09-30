37d1a0deff7362e3fc9c126b75281fc3ebe5d43e
nhz94259 <nhz94259@163.com>
2020-09-30 18:47:09 +0800

    Update reconfig.sh

--- zookeeper/secutity/reconfig.sh
+++ zookeeper/secutity/reconfig.sh
@@ -2,10 +2,10 @@
 #安装目录
 ZK_HOME="/Users/nahongze/Data/component/zookeeper/zookeeper_test"
 KAFKA_HOME="/Users/nahongze/Data/component/kafka/kafka_test"
-#访问用户\密码
+#访问用户名\密码
 SEC_USERNAME="admin"
 SEC_PASSWD="admin-2019"
-
+#复制jar到ZK lib下
 cp $KAFKA_HOME/libs/kafka-clients-*.jar $ZK_HOME/lib/
 cp $KAFKA_HOME/libs/lz4-java-*.jar $ZK_HOME/lib/
 cp $KAFKA_HOME/libs/slf4j-api-*.jar $ZK_HOME/lib/
@@ -13,14 +13,16 @@ cp $KAFKA_HOME/libs/slf4j-log4j12-*.jar $ZK_HOME/lib/
 cp $KAFKA_HOME/libs/snappy-java-*.jar $ZK_HOME/lib/
 echo finished copy jar
 
+#  conf/zoo.cfg 开启安全设置 配置文件末尾加 三行配置内容如下
 cat>>$ZK_HOME/conf/zoo.cfg<<EOF
 authProvider.1=org.apache.zookeeper.server.auth.SASLAuthenticationProvider
 requireClientAuthScheme=sasl
 jaasLoginRenew=3600000
 EOF
 echo zoo.cfg
-cat $ZK_HOME/conf/zoo.cfg  | grep -v ^# |grep -v ^$
+cat $ZK_HOME/conf/zoo.cfg
 
+# 新建 conf/zk_server_jaas.conf 并填充内容如下
 cat>$ZK_HOME/conf/zk_server_jaas.conf<<EOF
 Server {
     org.apache.kafka.common.security.plain.PlainLoginModule required 
@@ -33,10 +35,11 @@ EOF
 echo zk_server_jaas.cfg
 cat $ZK_HOME/conf/zk_server_jaas.conf
 
-
+# $ZK_HOME/bin/zkEnv.sh 脚本第二行添加 安全配置环境变量
 sed -i '2a\export SERVER_JVMFLAGS=" -Djava.security.auth.login.config=$ZK_HOME/conf/zk_server_jaas.conf "' $ZK_HOME/bin/zkEnv.sh
 cat $ZK_HOME/bin/zkEnv.sh  | grep -v ^# |grep -v ^$
 
+# 新建 $KAFKA_HOME/config/kafka_server_jaas.conf 并填充内容如下
 cat>$KAFKA_HOME/config/kafka_server_jaas.conf<<EOF
 KafkaServer {
        org.apache.kafka.common.security.plain.PlainLoginModule required
@@ -60,13 +63,15 @@ KafkaClient {
 };
 EOF
 cat $KAFKA_HOME/config/kafka_server_jaas.conf
-
+# 新建 $KAFKA_HOME/config/client-sasl.properties 并填充内容如下
 cat>$KAFKA_HOME/config/client-sasl.properties<<EOF
 security.protocol=SASL_PLAINTEXT
 sasl.mechanism=PLAIN
 EOF
 cat $KAFKA_HOME/config/client-sasl.properties
+# 替换$KAFKA_HOME/bin/kafka-server-start.sh KAFKA_HEAP_OPTS启动参数
 sed -i 's@export KAFKA_HEAP_OPTS="-Xmx1G -Xms1G"@export KAFKA_HEAP_OPTS=" -Xmx1G -Xms1G -Djava.security.auth.login.config=$KAFKA_HOME/config/kafka_server_jaas.conf"@'  $KAFKA_HOME/bin/kafka-server-start.sh
+# 替换$KAFKA_HOME/config/server.properties 开启kafka sasl安全配置
 sed -i 's@listeners=PLAINTEXT@listeners=SASL_PLAINTEXT@'  $KAFKA_HOME/config/server.properties
 sed -i "/listeners=SASL_PLAINTEXT/aallow.everyone.if.no.acl.found=true" $KAFKA_HOME/config/server.properties
 sed -i "/listeners=SASL_PLAINTEXT/aauthorizer.class.name=kafka.security.auth.SimpleAclAuthorizer" $KAFKA_HOME/config/server.properties
@@ -75,5 +80,3 @@ sed -i "/listeners=SASL_PLAINTEXT/asasl.enabled.mechanisms=PLAIN" $KAFKA_HOME/co
 sed -i "/listeners=SASL_PLAINTEXT/asecurity.inter.broker.protocol=SASL_PLAINTEXT" $KAFKA_HOME/config/server.properties
 
 cat $KAFKA_HOME/config/server.properties | grep -v ^# |grep -v ^$
-
-
